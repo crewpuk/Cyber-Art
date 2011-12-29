7622b9f5f114bf26e97d5ca53d8e217526628292
Rian <calledme.rian@gmail.com>
2011-12-30 01:39:29 +0700

    Adding jquery link

--- application/controllers/cyber_control.php
+++ application/controllers/cyber_control.php
@@ -12,6 +12,19 @@ class Cyber_control extends CI_Controller{
 		$data['css'] = link_tag('style/style.css');
 		$data['css'] .= link_tag('style/layout.css');
 		$data['css'] .= link_tag('style/class.css');
+        
+        /*
+         * Link_tag for jquery
+         */
+        $data['jquery_link'] = link_tag('js/jquery.js','','text/javascript');
+        $data['jquery_link'] .= link_tag('js/jquery_tools.js','','text/javascript');
+        $data['jquery_link'] .= link_tag('js/jquery_assets.js','','text/javascript');
+        // Adding jquery_link to css
+        $data['css'] .= $data['jquery_link'];
+        /*
+         * End of link_tag for jquery
+         */
+        
 		$data['art'] = $this->cyber_model->get_all_data('m_posting');
 		$this->load->view('main',$data);		
 	}
