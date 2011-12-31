1c75baa51ed3d07266f2999aaa3f803f048fffae
Rian <calledme.rian@gmail.com>
2011-12-31 17:20:09 +0700

    Tooltip

--- js/jquery_assets.js
+++ js/jquery_assets.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-    $('img[title]').tooltip();
+    $('img[title]').tooltip({
+
+	// place tooltip on the right edge
+	position: "center right",
+
+	// a little tweaking of the position
+	offset: [-2, 10],
+
+	// use the built-in fadeIn/fadeOut effect
+	effect: "fade",
+
+	// custom opacity setting
+	opacity: 0.7
+
+});
     $(".tab").each(function(){
         $(this).click(function(){
             tabelId = $(this).attr('id');
