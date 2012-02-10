8b591457df0cef688ef13d4058f82cdf2ec9dccf
Localhost <aryakusuma94@gmail.com>
2012-02-10 23:17:13 +0700

    Admin Detail Form

--- application/libraries/Admin_panel.php
+++ application/libraries/Admin_panel.php
@@ -110,21 +110,93 @@ class CI_Admin_panel{
 		return $table;
 	}
 	function tbl_input_agenda(){
-		$table = '
-			<h2>Agenda</h2><hr />
-		';	
+		$agenda = $this->CI->cyber_model->get_all_data('m_agenda',0,5);
+		$table = '<h2>Agenda</h2><hr />';
+		$table .= '<table width="100%" border="1" cellspacing="0" cellpadding="5">
+					  <tr>
+						<th>Nomor</th>
+						<th>Judul</th>
+						<th>Agenda</th>
+						<th>Tanggal Agenda</th>
+						<th>Tempat</th>
+						<th>Waktu</th>
+						<th>Pengirim</th>
+						<th>Tanggal Dibuat</th>
+						<th colspan="2">Tindakan</th>
+					  </tr>';
+		$i = 0;
+		foreach($agenda as $ag){
+  		$i++;
+		$table .= '	  <tr>
+						<td align="center">'.$i.'</td>
+						<td>'.$ag->title.'</td>
+						<td>'.$ag->agenda.'</td>
+						<td>'.$ag->tgl_agenda.'</td>
+						<td>'.$ag->tempat_agenda.'</td>
+						<td>'.$ag->waktu_agenda.'</td>
+						<td>'.$ag->pengirim.'</td>
+						<td>'.$ag->tanggal.'</td>
+						<td align="center"><img src="'.base_url().'images/ubah.png" width="16" height="16"></td>
+						<td align="center"><img src="'.base_url().'images/hapus.png" width="16" height="16"></td>
+					  </tr>';
+		}
+  		$table .= '</table>';
 		return $table;
 	}
 	function tbl_input_shout(){
-		$table = '
-			<h2>Shoutbox</h2><hr />
-		';	
+		$shoutbox = $this->CI->cyber_model->get_all_data('m_shoutbox',0,5);
+		$table = '<h2>Shoutbox</h2><hr />';	
+		$table .= '<table width="100%" border="1" cellspacing="0" cellpadding="5">
+					  <tr>
+						<th>Nomor</th>
+						<th>Nama</th>
+						<th>Url</th>
+						<th>Pesan</th>
+						<th>Waktu</th>
+						<th>Tindakan</th>
+					  </tr>';
+		$i = 0;
+		foreach($shoutbox as $sb){
+		$i++;
+  		$table .= '	  <tr>
+						<td align="center">'.$i.'</td>
+						<td>'.$sb->nama.'</td>
+						<td>'.$sb->url.'</td>
+						<td>'.$sb->pesan.'</td>
+						<td>'.$sb->time_stamp.'</td>
+						<td align="center"><img src="'.base_url().'images/hapus.png" width="16" height="16"></td>
+					  </tr>';
+		}
+  		$table .= '</table>';
 		return $table;
 	}
 	function tbl_input_komentar(){
-		$table = '
-			<h2>Komentar</h2><hr />
-		';	
+		$komentar = $this->CI->cyber_model->get_all_data('m_komentar',0,5);
+		$table = '<h2>Komentar</h2><hr />';
+		$table .= '<table width="100%" border="1" cellspacing="0" cellpadding="5">
+					  <tr>
+						<th>Nomor</th>
+						<th>Id Komentar</th>
+						<th>Nama</th>
+						<th>Website</th>
+						<th>Isi</th>
+						<th>Tanggal</th>
+						<th>Tindakan</th>
+					  </tr>';
+		$i = 0;
+		foreach($komentar as $km){
+		$i++;
+  		$table .= '   <tr>
+						<td align="center">'.$i.'</td>
+						<td align="center">'.$km->id_komentar.'</td>
+						<td>'.$km->nama.'</td>
+						<td>'.$km->website.'</td>
+						<td>'.$km->isi.'</td>
+						<td>'.$km->tanggal.'</td>
+						<td align="center"><img src="'.base_url().'images/hapus.png" width="16" height="16"></td>
+					  </tr>';
+		}
+  		$table .= '</table>';
 		return $table;
 	}
 }
